000a4839400cbda6c00c2f1e343e14889ba5a7ca
juhui <juya202106@naver.com>
2025-04-30 11:54:08 +0900

    js function,scope 추가

---
+++ 12_JUHUI/01_javascript-core-lecture-source/04_function/05_first-class-object/01_first-class-object.js
@@ -0,0 +1,24 @@
+//  일급 객체
+// 1. 무명의 리터럴로 생성할 수 있다. 즉, 런타임에 생성이 가능하다.
+// 2. 변수나 자료구조(객체, 배열 등)에 저장할 수 있다.
+
+var hello = function () {
+  return "안녕!";
+};
+
+var obj = { hello }; // 리터럴로 만든 객체고, 객체 축약한 내용이고 함수명도 hello, 속성?도 hello
+
+// 3. 함수의 매개변수에 전달할 수 있다.
+// 4. 함수의 반환값으로 사용할 수 있다.
+function repeat(func, count) {
+  for (var i = 0; i < count; i++) {
+    console.log(func());
+  }
+  return function () {
+    console.log(`${count}번 반복환료`);
+  };
+}
+
+repeat(obj.hello, 10);
+var returnFunc = repeat(obj.hello, 10);
+returnFunc();
