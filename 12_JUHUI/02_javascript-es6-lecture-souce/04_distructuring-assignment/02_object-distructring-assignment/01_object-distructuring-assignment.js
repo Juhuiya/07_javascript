362f37197e7ef391d02518bad526c3194b8ed96e
juhui <juya202106@naver.com>
2025-05-07 12:01:21 +0900

    4. 구조분해할당마침

---
+++ 12_JUHUI/02_javascript-es6-lecture-souce/04_distructuring-assignment/02_object-distructring-assignment/01_object-distructuring-assignment.js
@@ -0,0 +1,21 @@
+// 객체 구조 분해 할당. 잘 기억하기
+const pants = {
+  productName: "배기팬츠",
+  color: "검정색",
+  price: 30000,
+}; // 리터럴로 객체 생성
+
+console.log(pants);
+const { productName, price, color } = pants; // 객체라 순서변경은 가능, 분해할 때 줄 때 이름이 같은 애를 찾는건데 이름이 다르면 undefined 일어남.
+console.log(productName);
+console.log(price);
+console.log(color);
+
+// 각 변수의 
+// 프로퍼티 키값과 다른 변수명을 설정할 수 있다.
+
+//먼저 작성. 이름을 변경하고 싶다면 :(콜론)을 이용해서 변경할 수 있다.
+const { productName: pn, price: pr, color: co } = pants;
+console.log(pn);
+console.log(pr);
+console.log(co);
---
+++ 12_JUHUI/02_javascript-es6-lecture-souce/04_distructuring-assignment/02_object-distructring-assignment/04_fuction-parameter.js
@@ -0,0 +1,28 @@
+// 함수의 매개변수 :  함수의 매개변수가 많거나 기본값이 필요한 경우 사용한다.
+function displayProdect(
+  producer = "아무개",
+  width = 0,
+  height = 0,
+  items = []
+) {}
+
+// 인수의 순서가 고정되어 있기 대문에 순서가 바뀌면 안된다.
+// 또한 기본값을 활용하더라도 undefined를 맞추어서 인수 자리에 넘겨주어야 한다.
+displayProdect("신사임당", undefined, undefined, ["Coffee", "Donut"]);
+
+function displayProdect2({
+  // 인자 하나에 여러개가 담겨져있음.
+  producer = "아무개",
+  width = 0,
+  height = 0,
+  items = [],
+}) {
+  console.log(producer);
+  console.log(width);
+  console.log(height);
+  console.log(items);
+}
+
+// 인수의 순서도 상관이 없고 값을 넘기지 않아도 기본값이 적용된다.
+const example = { producer: "홍길동", items: ["doll", "robot"] };
+displayProdect2(example);
